f22b73d5b723e96d7fd40a4ed7231364a0f01932
tomasharten <thartenstein@udesa.edu.ar>
2022-11-18 11:36:19 -0300

    Detail generos

--- js/detail_genero.js
+++ js/detail_genero.js
@@ -1,12 +1,16 @@
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
 let genero = qsObj.get("detallegenero");
+let nameGenre = qsObj.get("name");
+
 let apiKey= "371e304b1b9f8df6a3f0e225dc4511b7";
 let urlgenero= `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genero}&with_watch_monetization_types=flatrate`
 
 let sectiongenero= document.querySelector(".conteiner")
 let variostitulosprincipales= document.querySelector(".variostitulosprincipales");
+let em = document.querySelector ('.em')
 
+em.innerText = 'Genero: ' + nameGenre
 fetch(urlgenero)
 .then(function (response) {
     return response.json()
@@ -20,15 +24,14 @@ fetch(urlgenero)
         let imagen= data.results[i].poster_path;
         let titulo= data.results[i].title;
         let id = data.results[i].id
-
+    
         pelisdelgenero += `<article class="portadaFavoritos"> 
-        <a href="./detail_series.html?idPersonaje=${id}">
-            <img class= "alvin1" src="${imagen}" alt="">
+        <a href="./detail_movie.html?idPersonaje=${id}">
+            <img class= "alvin1" src="https://image.tmdb.org/t/p/w500${imagen}">
             <h3 class="texto-info">${titulo}</h3>
         </a>
     </article>`        
     }
-
 
     sectiongenero.innerHTML= pelisdelgenero;
     return data
